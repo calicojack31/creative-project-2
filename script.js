c84147cc04e3fc9c88adc5a898f3a9ebf0cb601d
Zach Westhoff <zrwesthoff@gmail.com>
2020-02-20 11:11:25 -0700

    cleaned things up, added links to results

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-/*
-function doesFileExist(urlToFile) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('HEAD', urlToFile, false);
-  xhr.send();
-
-  if (xhr.status == "404") {
-    return false;
-  } else {
-    return true;
-  }
-}
-*/
-
-
 document.getElementById("cultureSubmit").addEventListener("click", function(event) {
   event.preventDefault();
   const value = document.getElementById("cultureInput").value;
@@ -20,17 +5,17 @@ document.getElementById("cultureSubmit").addEventListener("click", function(even
     return;
   console.log(value);
 
-  //const url = "http://api.openweathermap.org/data/2.5/weather?q=" + value + ",US&units=imperial" + "&APPID=7cdcb7998d2273d430549d3fdd569083";
-  //const url = "http://tastedive.com/api/similar?q=" + value + "&k=356160-Computer-AHFW7BJ2";
   const url = "https://api.harvardartmuseums.org/object?q=people.culture:" + value + "&apikey=158f3020-52c2-11ea-b8be-c9817d3722a4";
   fetch(url)
     .then(function(response) {
       return response.json();
     }).then(function(json) {
       let results = "";
-      results += "<h2>Here is your selection of " + value + " artworks!</h2>";
-      //NEED TO MAKE THIS A LOOP, for records.length
-      //NEED SOME WAY TO MAKE PHOTOS SIZED BETTER
+      if (json.records.length === 0) {
+        results += "<h2>Sorry, we couldn't find any " + value + " artworks.</h2>";
+      } else {
+        results += "<h2>Here is your selection of " + value + " artworks!</h2>";
+      }
       results += '<div class="row">';
       for (let i = 0; i < json.records.length; i++) {
         if (json.records[i].primaryimageurl === null || json.records[i].primaryimageurl === undefined) {
@@ -46,59 +31,14 @@ document.getElementById("cultureSubmit").addEventListener("click", function(even
           } else {
             results += json.records[i].technique + "</h4>";
           }
+          results += "<h4> <a href='" + json.records[i].url + "'> View Page at Harvard</a></h4>";
           results += "<h4>" + "Page views: " + json.records[i].totalpageviews + "</h4>";
           results += '</div>';
         }
       }
       results += '</div>';
 
-      //results += "<h4>" + json.similar.info.name + "<h4>";
-      /*
-      results += '<h2 class="section-title">Weather in ' + json.name + " as of " + moment(json.dt_txt).format('MMMM Do YYYY, h:mm a') + "</h2>";
-      for (let i = 0; i < json.weather.length; i++) {
-        results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png" class="weather-icon"/>';
-        results += "<p class='weather-description'>" + json.weather[i].main + ", " + json.clouds.all + "% Cloudy</p>";
-      }
-      results += '<h4>' + "Temperature: " + json.main.temp + " &deg;F</h4>"; //I added the semicolon
-      results += "<h4>" + "Feels Like: " + json.main.feels_like + " &deg;F</h4>";
-      results += "<h4>" + "High: " + json.main.temp_max + " &deg;F</h4>";
-      results += "<h4>" + "Low: " + json.main.temp_min + " &deg;F</h4>";
-      results += "<h4>" + "Humidity: " + json.main.humidity + "%</h4>";
-      results += "<h4>" + "Wind Speed: " + json.wind.speed + " m/s</h4>";
-      results += "<h4>" + "Wind Direction: ";
-      if (json.wind.deg === undefined) {
-        results += "Presently unavailable for this location</h4>";
-      } else {
-        results += json.wind.deg + "&deg;</h4>";
-      }
-      */
       document.getElementById("cultureResults").innerHTML = results;
     });
-
-  /*
-    const url2 = "http://api.openweathermap.org/data/2.5/forecast?q=" + value + ", US&units=imperial" + "&APPID=7cdcb7998d2273d430549d3fdd569083";
-    fetch(url2)
-      .then(function(response) {
-        return response.json();
-      }).then(function(json) {
-        let forecast = "";
-        forecast += "<h2 class='section-title'>Five-day/Three-Hour Forecast for " + json.city.name + "</h2>";
-        forecast += '<div class ="row">';
-        for (let i = 0; i < json.list.length; i++) {
-          forecast += '<div class="col-sm">';
-          forecast += "<h5>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY, h:mm a') + "</h5>";
-          forecast += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png" class="weather-icon"/>';
-          forecast += "<p class='weather-description'>" + json.list[i].weather[0].main + ", " + json.list[i].clouds.all + "% Cloudy</p>";
-          forecast += "<p>Temperature: " + json.list[i].main.temp + " &deg;F</p>";
-          forecast += "<p>High: " + json.list[i].main.temp_max + " &deg;F</p>";
-          forecast += "<p>Low: " + json.list[i].main.temp_min + " &deg;F</p>";
-          forecast += "<p>Humidity: " + json.list[i].main.humidity + "%</p>";
-          forecast += "<p>Wind Speed: " + json.list[i].wind.speed + " m/s</p>";
-          forecast += '</div>';
-        }
-        forecast += '</div>';
-        document.getElementById("forecastResults").innerHTML = forecast;
-      });
-  */
 
 });
